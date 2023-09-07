5ce04abd527595ff3c6e0237387a63dfef7993da
NattyNgrn <natnrgn@gmail.com>
2023-09-07 18:25:47 -0400

    setting up quiz component

---
+++ client/src/components/quiz.jsx
@@ -0,0 +1,19 @@
+function Quiz() {
+    const [question, setquestion] = useState({
+        question: "",
+        a1: "",
+        a2: "",
+        a3: "",
+        a4: "",
+        correct: ""
+    })
+
+    return (
+        <div>
+
+
+        </div>
+    )
+}
+
+export default Quiz
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import pg from "pg";
+
 
 const app = express();
 app.use(cors());
@@ -8,18 +8,14 @@ app.use(cors());
 const API = "https://opentdb.com/api.php?amount=10&category=27&difficulty=medium&type=multiple";
 const port = process.env.PORT || 7777;
 
-async function userPass() {
-
-}
-
 async function getQuiz(){
     const response = await fetch (API);
     const data = await response.json();
     return data;
 }
 
-app.get("/users", (req, res) => {
-    res.json("USER AND PASSWORD");
+app.get("/", (req, res) => {
+    res.json("WELCOME");
 });
 
 app.get("/quiz", (req, res) => {
