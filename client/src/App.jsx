08dd122ab7b0437dd44615d8be1e8ccde47f4a65
NattyNgrn <natnrgn@gmail.com>
2023-09-07 18:31:55 -0400

    importing component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Quiz from "./components/quiz"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
       <div>
-        
+        <h1>Test your animal knowledge!</h1>
+        <Quiz/>
       </div>
   )
 }
