39b1e8b8eab1e097ee2853e896cf8aabdbebd0dc
NattyNgrn <natnrgn@gmail.com>
2023-09-07 22:08:29 -0400

    ugh

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,11 @@ import './App.css'
 import Quiz from "./components/quiz"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
       <div>
         <h1>Test your animal knowledge!</h1>
-        <Quiz {...Quiz}/>
+        <Quiz/>
       </div>
   )
 }
