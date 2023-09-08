284ce263c09c4f7bc8b19a92e42ef59f7e4e7e76
NattyNgrn <natnrgn@gmail.com>
2023-09-07 21:30:59 -0400

    finished get quiz asyn func

--- server/server.js
+++ server/server.js
@@ -9,22 +9,19 @@ const API = "https://opentdb.com/api.php?amount=10&category=27&difficulty=medium
 const port = process.env.PORT || 7777;
 
 async function getQuiz(API){
-    let random = Math.floor(Math.random() * 10) + 1;
-    let randomIndex = Math.floor(Math.random() * 2) + 1;
-    const response = await fetch (API);
+    let random = Math.floor(Math.random() * 10);
+    let randomIndex = Math.floor(Math.random() * 2);
+    const response = await fetch(API);
     const data = await response.json();
     console.log(data);
     let answers = data["results"][random]["incorrect_answers"];
     answers.splice(randomIndex, 0, data["results"][random]["correct_answer"])
     const quiz = {
         question: data["results"][random]["question"],
-        answer: "",
-        a2: "",
-        a3: "",
-        a4: "",
-        correct: ""
+        answers: answers,
+        correct: data["results"][random]["correct_answer"]
     }
-
+    return quiz;
 }
 
 app.get("/", (req, res) => {
