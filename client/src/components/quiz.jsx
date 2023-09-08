cc6de51f7caf3f663a7061510799017492d01bbb
NattyNgrn <natnrgn@gmail.com>
2023-09-07 22:08:36 -0400

    ugh

--- client/src/components/quiz.jsx
+++ client/src/components/quiz.jsx
@@ -22,10 +22,10 @@ function Quiz() {
         <div>
 
         <h1>{question.question}</h1>
-        <button onClick={checkAnswer(question.answers[0])}>{question.answers[0]}</button>
-        <button onClick={checkAnswer(question.answers[1])}>{question.answers[1]}</button>
-        <button onClick={checkAnswer(question.answers[2])}>{question.answers[2]}</button>
-        <button onClick={checkAnswer(question.answers[3])}>{question.answers[3]}</button>
+        <button onClick={checkAnswer}>{question.answers[0]}</button>
+        <button onClick={checkAnswer}>{question.answers[1]}</button>
+        <button onClick={checkAnswer}>{question.answers[2]}</button>
+        <button onClick={checkAnswer}>{question.answers[3]}</button>
         {/*<button type="submit" onClick={checkAnswer}>Check answer!</button>*/}
         <button type="submit" onClick={getQuestion}>Get new question!</button>
         <h2>{checkAnswer}</h2>
